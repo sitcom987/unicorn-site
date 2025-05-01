d660d6cd19c40a4ec1793cef5cadd352293bd0d7
sitcom987 <szkl.adam@gmail.com>
2025-05-01 23:57:06 +0200

    first animations

--- js.js
+++ js.js
@@ -31,76 +31,46 @@ document.addEventListener("DOMContentLoaded", function () {
         scrub: 1,
     });
 
-    //Font weight
+    gsap.from(".fade", {duration: 3, opacity: 0, ease: "power2", stagger: 0.4, });
 
-    const animateChars = (chars, reverse = false) => {
-        const staggerOptions = {
-            each: 0.35,
-            from: reverse ? "start" : "end",
-            ease: "linear",
-        };
+    function init(){
 
-        gsap.fromTo(chars, {
-            fontWeight: 100,}, {
-            fontWeight: 800,
-                duration: 1,
-                ease: "none",
-                stagger: staggerOptions,
-                scrollTrigger: {
-                    trigger: chars[0].closet(".leftcontainer"),
-                    start: 50% bottom,
-                    end: "top top",
-                    scrub: true,
-                },
-        });
-
-    };
-
-    const splitText = new SplitType(".leftcard h2", {types: "chars"});
-
-    const cardContainers = document.querySelectorAll(".leftcard");
-
-    cardContainers.forEach((explainercards, index) => {
-        let start = "0%";
-        let end = "-15%";
-
-        if (index % 2 === 0) {
-            start = "0%";
-            end = "10%";
-        }
-
-        const card = explainercards.querySelector(".leftcontainer");
-        const words = explainercards.querySelector(".leftcard h2");
-
-        gsap.fromTo(card, {
-            x: start, 
-        }, {
-            x: end,
+        gsap.to(".fade", {opacity: 0, duration: 2, 
             scrollTrigger: {
-                trigger: explainercards,
+                trigger: ".fade",
                 start: "top bottom",
-                end: "150% top",
-                scrub: true,
-            },
-
-        });
-
-        words.forEach((word) => {
-            const chars = Array.from(word.querySelectorAll(".char"));
-            if (chars.length) {
-                const reverse = index % 2 !== 0;
-                animateChars(chars, reverse);
+                end: "bottom bottom",
+                toggleActions: "restart pause resume restart",
+                scrub: 1,
             }
         });
 
-    });
+        const h1 = gsap.utils.toArray("h1");
+        h1.forEach(h1 => {
+            gsap.from(h1, {
+                opacity: 0,
+                duration: 2,
+                yPercent: 30,
+                scrub: 1.5,
+                scrollTrigger: {
+                    trigger: h1,
+                    start: "top bottom-=10%",
+                    end: "top center",
+                    toggleActions: "play none none reverse",
+                }
+            })
+        });
+
+        
+
+    } //init
 
 
     //Lenis smooth scroll
     
-    const lenis = new Lenis();
+    /* const lenis = new Lenis();
     lenis.on("scroll", ScrollTrigger.update);
     gsap.ticker.add((time) => lenis.raf(time * 1000));
-    gsap.ticker.lagSmoothing(0);
+    gsap.ticker.lagSmoothing(0); */
 
 });
